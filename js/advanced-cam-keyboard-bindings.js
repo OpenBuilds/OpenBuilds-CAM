9aa744e5811387fe8685abddf37fa05060f0d004
openbuilds-engineer <p.walt@openbuilds.com>
2018-12-27 18:28:33 +0200

    Fix for delete/backspace behaviour in dialogs

--- js/advanced-cam-keyboard-bindings.js
+++ js/advanced-cam-keyboard-bindings.js
@@ -1,35 +1,40 @@
 $(document).keydown(function(e) {
-  // console.log(e)
-  if (e.which == 46 || e.which == 8) { // Press delete to delete whatever is already selected (8=backspace = the delete key on a Mac keyboard (smh) )
-    // console.log('del');
-    if (mouseState != "scale") {
-      storeUndo(true);
-      deleteSelectedObjects();
+  var isModalOpen = Metro.dialog.isOpen('#statusmodal')
+
+  if (!isModalOpen) {
+    // console.log(e)
+    if (e.which == 46 || e.which == 8) { // Press delete to delete whatever is already selected (8=backspace = the delete key on a Mac keyboard (smh) )
+      // console.log('del');
+      if (mouseState != "scale") {
+        storeUndo(true);
+        deleteSelectedObjects();
+        return false;
+      }
+    } else if (e.which === 90 && e.ctrlKey && e.shiftKey) { // Press ctrl+shift+z to Redo after undo
+      // console.log('control + shift + z');
+      redo();
       return false;
-    }
-  } else if (e.which === 90 && e.ctrlKey && e.shiftKey) { // Press ctrl+shift+z to Redo after undo
-    // console.log('control + shift + z');
-    redo();
-    return false;
-  } else if (e.which === 90 && e.ctrlKey) { // Press ctrl+z to undo
-    // console.log('control + z');
-    undo()
-    return false;
-  } else if (e.which === 89 && e.ctrlKey) { // Press ctrl+y to Redo after undo (same as ctrl+shift+i)
-    // console.log('control + y');
-    redo()
-    return false;
-  } else if (e.which === 65 && e.ctrlKey && e.shiftKey) { // Press ctrl+shift+a Unselect All
-    // console.log('control + shift + a');
-    if (mouseState != "scale") {
-      selectNone();
+    } else if (e.which === 90 && e.ctrlKey) { // Press ctrl+z to undo
+      // console.log('control + z');
+      undo()
       return false;
+    } else if (e.which === 89 && e.ctrlKey) { // Press ctrl+y to Redo after undo (same as ctrl+shift+i)
+      // console.log('control + y');
+      redo()
+      return false;
+    } else if (e.which === 65 && e.ctrlKey && e.shiftKey) { // Press ctrl+shift+a Unselect All
+      // console.log('control + shift + a');
+      if (mouseState != "scale") {
+        selectNone();
+        return false;
+      };
+    } else if (e.which === 65 && e.ctrlKey) { // Press ctrl+a to Select All
+      // console.log('control + a');
+      if (mouseState != "scale") {
+        selectAll();
+        return false;
+      };
     };
-  } else if (e.which === 65 && e.ctrlKey) { // Press ctrl+a to Select All
-    // console.log('control + a');
-    if (mouseState != "scale") {
-      selectAll();
-      return false;
-    };
-  };
+  }
+
 });
