1f109e50cbf339c93245db26dba1f5c17f441843
openbuilds-engineer <p.walt@openbuilds.com>
2019-01-08 17:31:01 +0200

    Laser Hello World

--- js/basic-cad-helloworld.js
+++ js/basic-cad-helloworld.js
@@ -12,3 +12,18 @@ function helloWorld() {
     }, 3000)
   });
 }
+
+function helloWorldLaser() {
+  $.get("./workspace/helloworldlaser.json", function(data) {
+    console.log(data)
+    parseLoadWorkspace(data)
+    printLog('HelloWorld-Laser Opened', msgcolor, "file");
+    // putFileObjectAtZero();
+    resetView();
+    // $("#partslibModal").modal('hide')
+    fillTree();
+    setTimeout(function() {
+      makeGcode();
+    }, 3000)
+  });
+}
