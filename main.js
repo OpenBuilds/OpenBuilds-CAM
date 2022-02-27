47c4999bf6d4116467b49b666b50da6add64b063
Bob Wood <rlwoodjr@gmail.com>
2022-02-27 16:29:46 -0600

    v1.1.6

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const createWindow = () => {
     const mainWindow = new BrowserWindow({
         width: 1025,
         height: 850,
-        autoHideMenuBar: true,
+        autoHideMenuBar: true
     });
 
     mainWindow.loadFile('electron.html');
